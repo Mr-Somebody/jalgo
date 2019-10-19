374069543dc84200913fb44b7e27ccaf8b7ea496
Joey Liu <joey.liu@rokt.com>
2019-10-19 12:13:15 +1100

    fix problems

--- js/creative.js
+++ js/creative.js
@@ -40,7 +40,7 @@
   $(window).scroll(navbarCollapse);
 
   // Magnific popup calls
-  $('#portfolio').magnificPopup({
+  $('.portfolio').magnificPopup({
     delegate: 'a',
     type: 'image',
     tLoading: 'Loading image #%curr%...',
